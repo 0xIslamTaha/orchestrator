37a4c26b45f0d6142fb826a718e7fa9be082341b
0xIslamTaha <islamtaha2012@gmail.com>
2021-08-17 18:50:04 +0200

    - apply the comments - enhance the code.

--- src/analyseReport.js
+++ src/analyseReport.js
@@ -1,71 +1,42 @@
 const fs = require("fs");
-
 let specs = [];
 const browsers = ["chrome", "firefox"];
 const defaultBrowser = "chrome"; // I will use it in case of there is no browser in the title.
 
 function getBrowserFromTitle(title) {
-  title = title.toLowerCase();
-  for (let browser of browsers) {
-    if (title.includes(browser)) {
-      return browser;
-    }
-  }
+  return browsers.find(browser => title.toLowerCase().includes(browser))
 }
 
 function orderBasedOnBrowserDuration(browser) {
-  let aDuration, bDuration;
   return specs.sort(function (a, b) {
-    for (let i = 0; i < a.data.length; i++) {
-      if (a.data[i].browser === browser) {
-        aDuration = a.data[i].duration;
-        break;
-      }
-    }
-
-    for (let i = 0; i < b.data.length; i++) {
-      if (b.data[i].browser === browser) {
-        bDuration = b.data[i].duration;
-        break;
-      }
-    }
-
+    let aDuration = a.data.find( item => item.browser === browser ).duration
+    let bDuration = b.data.find( item => item.browser === browser ).duration
     return aDuration - bDuration;
   });
 }
 
 function intiateSpecData(specName) {
-  for (let spec of specs) {
-    if (spec["specName"] === specName) {
-      return;
-    }
+  if (!specs.find( spec => spec.specName === specName )) {
+    specs.push({
+      specName: specName,
+      data: browsers.map((browser) => {
+        return { browser: browser, duration: 0 };
+      }),
+    });
   }
-  specs.push({
-    specName: specName,
-    data: browsers.map((browser) => {
-      return { browser: browser, duration: 0 };
-    }),
-  });
 }
 
 function updateSpecData(suites, specName) {
-  for (let suite of suites) {
+  suites.forEach(suite => {
     let browser = getBrowserFromTitle(suite["title"]) || defaultBrowser;
     let duration = parseInt(suite["duration"]);
-    for (let spec of specs) {
-      if (spec["specName"] === specName) {
-        spec.data.forEach((data) => {
-          if (data.browser === browser) {
-            data.duration += duration;
-          }
-        });
-      }
-    }
 
+    let spec = specs.find( spec => spec.specName === specName );
+    if (spec) spec.data.find( item => item.browser === browser ).duration += duration;
     if (suite.hasOwnProperty("suites")) {
       updateSpecData(suite.suites, specName);
     }
-  }
+  })
 }
 
 function checkReportIsExisting(reportPath) {
