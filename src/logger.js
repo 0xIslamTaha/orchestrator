9899d13fe02678f437ce947ce0cb68866f0f4a2e
0xIslamTaha <islamtaha2012@gmail.com>
2022-01-19 19:57:43 +0200

    release(2.0.1): enhance the log messages [DS-1275]

---
+++ src/logger.js
@@ -0,0 +1,30 @@
+function banner() {
+  let banner = `
+   ██████      ██████       ██████     ██   ██     ███████     ███████     ████████     ██████       █████      ████████      ██████      ██████  
+  ██    ██     ██   ██     ██          ██   ██     ██          ██             ██        ██   ██     ██   ██        ██        ██    ██     ██   ██ 
+  ██    ██     ██████      ██          ███████     █████       ███████        ██        ██████      ███████        ██        ██    ██     ██████  
+  ██    ██     ██   ██     ██          ██   ██     ██               ██        ██        ██   ██     ██   ██        ██        ██    ██     ██   ██ 
+   ██████      ██   ██      ██████     ██   ██     ███████     ███████        ██        ██   ██     ██   ██        ██         ██████      ██   ██ 
+                                                                                                                             v 2.0.1 @0xIslamTaha                                                                                                                                                                                                                                                                                
+`;
+
+  console.log(banner);
+}
+
+
+function step(msg, newLine=false) {
+  let message;
+  if (newLine) {
+    message = `\n[*] ${msg}`; 
+  } else {
+    message = `[*] ${msg}`;
+  }
+  console.log(message);
+}
+
+function subStep(subStep){
+    let message = `[-] ${subStep}`;
+    console.log(message);
+}
+
+export { banner, step, subStep };
